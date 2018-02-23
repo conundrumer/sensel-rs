45714f30444f13ccc36aa39d2fa20a6b76085da1
David Lu <davidelu93@gmail.com>
2018-02-23 02:59:10 -0500

    handle checking valid method calls at compile time

--- src/bin/api_test.rs
+++ src/bin/api_test.rs
@@ -1,15 +1,18 @@
 extern crate sensel;
 
 use std::{thread, time};
+use sensel::device::Device;
 
 fn main() {
     loop {
         match run() {
             Ok(tested) => if tested { break },
-            Err(_) => println!("Something went wrong with the device")
+            Err(_) => {
+                println!("Something went wrong with the device");
+                break
+            }
         }
         thread::sleep(time::Duration::from_millis(1000));
-        println!()
     }
 }
 
@@ -29,84 +32,84 @@ fn run() -> Result<bool, sensel::SenselError> {
 
     let device = device_id.open()?;
 
-    println!("{:?}", device.sensor_info);
-    println!("{:?}", device.fw_info);
-    println!("num_leds: {}", device.num_leds);
-    println!("max_led_brightness: {}", device.max_led_brightness);
-    println!("supported frame content: {:?}", device.supported_frame_content);
+    println!("{:?}", device.get_info());
 
     // thread::sleep(time::Duration::from_millis(1000));
 
     let pressed = device.get_power_button_pressed()?;
     println!("power_button_pressed: {}", pressed);
 
-    let tests: Vec<Box<Fn() -> Result<_, _>>> = vec![Box::new(|| {
-        device.set_led_brightness(0, 100)?;
-        assert!(device.get_led_brightness(0)? == 100);
-        Ok(())
-    }), Box::new(|| {
-        device.set_scan_mode(sensel::scan_mode::SCAN_MODE_ASYNC)?;
-        assert!(device.get_scan_mode()? == sensel::scan_mode::SCAN_MODE_ASYNC);
-        Ok(())
-    }), Box::new(|| {
-        device.set_scan_detail(sensel::scan_detail::SCAN_DETAIL_LOW)?;
-        assert!(device.get_scan_detail()? == sensel::scan_detail::SCAN_DETAIL_LOW);
-        Ok(())
-    }), Box::new(|| {
-        device.set_buffer_control(42)?;
-        assert!(device.get_buffer_control()? == 42);
-        Ok(())
-    }), Box::new(|| {
-        device.set_max_frame_rate(42)?;
-        assert!(device.get_max_frame_rate()? == 42);
-        Ok(())
-    }), Box::new(|| {
-        device.set_frame_content(sensel::frame::Mask::ACCEL)?;
-        assert!(device.get_frame_content()? == sensel::frame::Mask::ACCEL);
-        Ok(())
-    }), Box::new(|| {
-        device.set_contacts_mask(sensel::contact::Mask::PEAK)?;
-        assert!(device.get_contacts_mask()? == sensel::contact::Mask::PEAK);
-        Ok(())
-    }), Box::new(|| {
-        device.set_contacts_min_force(42)?;
-        assert!(device.get_contacts_min_force()? == 42);
-        Ok(())
-    }), Box::new(|| {
-        device.set_contacts_enable_blob_merge(false)?;
-        assert!(device.get_contacts_enable_blob_merge()? == false);
-        Ok(())
-    }), Box::new(|| {
-        device.set_dynamic_baseline_enabled(false)?;
-        assert!(device.get_dynamic_baseline_enabled()? == false);
-        Ok(())
-    })];
+    {
+        let tests: Vec<Box<Fn() -> Result<_, _>>> = vec![Box::new(|| {
+            device.set_led_brightness(0, 100)?;
+            assert!(device.get_led_brightness(0)? == 100);
+            Ok(())
+        }), Box::new(|| {
+            device.set_scan_mode(sensel::scan_mode::SCAN_MODE_ASYNC)?;
+            assert!(device.get_scan_mode()? == sensel::scan_mode::SCAN_MODE_ASYNC);
+            Ok(())
+        }), Box::new(|| {
+            device.set_scan_detail(sensel::scan_detail::SCAN_DETAIL_LOW)?;
+            assert!(device.get_scan_detail()? == sensel::scan_detail::SCAN_DETAIL_LOW);
+            Ok(())
+        }), Box::new(|| {
+            device.set_buffer_control(42)?;
+            assert!(device.get_buffer_control()? == 42);
+            Ok(())
+        }), Box::new(|| {
+            device.set_max_frame_rate(42)?;
+            assert!(device.get_max_frame_rate()? == 42);
+            Ok(())
+        }), Box::new(|| {
+            device.set_frame_content(sensel::frame::Mask::ACCEL)?;
+            assert!(device.get_frame_content()? == sensel::frame::Mask::ACCEL);
+            Ok(())
+        }), Box::new(|| {
+            device.set_contacts_mask(sensel::contact::Mask::PEAK)?;
+            assert!(device.get_contacts_mask()? == sensel::contact::Mask::PEAK);
+            Ok(())
+        }), Box::new(|| {
+            device.set_contacts_min_force(42)?;
+            assert!(device.get_contacts_min_force()? == 42);
+            Ok(())
+        }), Box::new(|| {
+            device.set_contacts_enable_blob_merge(false)?;
+            assert!(device.get_contacts_enable_blob_merge()? == false);
+            Ok(())
+        }), Box::new(|| {
+            device.set_dynamic_baseline_enabled(false)?;
+            assert!(device.get_dynamic_baseline_enabled()? == false);
+            Ok(())
+        })];
 
-    for (i, test) in tests.iter().enumerate() {
-        test()?;
-        device.set_led_brightness(i as u8, 100)?;
+        for (i, test) in tests.iter().enumerate() {
+            test()?;
+            device.set_led_brightness(i as u8, 100)?;
+        }
     }
 
-    device.set_led_array(&vec![100; device.num_leds])?;
+    device.set_led_array(&vec![100; device.get_info().num_leds])?;
 
     device.soft_reset()?;
 
     device.set_frame_content(sensel::frame::Mask::CONTACTS | sensel::frame::Mask::ACCEL)?;
 
-    device.start_scanning()?; // TODO: make sensor struct
+    let scanning_device = device.start_scanning()?;
 
-    device.read_sensor()?;
+    scanning_device.read_sensor()?;
 
-    let num_frames = device.get_num_available_frames()?;
+    let num_frames = scanning_device.get_num_available_frames()?;
 
     for _ in 0..num_frames {
-        let frame = device.get_frame()?;
+        let frame = scanning_device.get_frame()?;
         println!("{:?}", frame)
     }
 
-    device.stop_scanning()?; // TODO: consume sensor struct
+    scanning_device.set_led_array(&vec![0; scanning_device.get_info().num_leds])?;
 
-    device.set_led_array(&vec![0; device.num_leds])?;
+    let device = scanning_device.stop_scanning()?;
+
+    let _ = device;
 
     // implicitly closed when dropped
     // device.close();
--- src/device.rs
+++ src/device.rs
@@ -13,16 +13,28 @@ enum LEDArray {
 
 pub struct DeviceList(SenselDeviceList);
 
-pub struct Device {
-    handle: *mut ::std::os::raw::c_void,
-    frame_data: *mut SenselFrameData,
-    pub id: SenselDeviceID,
+impl SenselSensorInfo {
+    pub fn get_num_sensors(&self) -> usize {
+        self.num_rows as usize * self.num_cols as usize
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct DeviceInfo {
     pub sensor_info: SenselSensorInfo,
     pub fw_info: SenselFirmwareInfo,
     pub supported_frame_content: frame::Mask,
     pub num_leds: usize,
     pub max_led_brightness: u16,
-    led_array_buf: RefCell<LEDArray>
+}
+
+pub struct ScanningDevice(BaseDevice);
+
+pub struct BaseDevice {
+    handle: *mut ::std::os::raw::c_void,
+    frame_data: *mut SenselFrameData,
+    led_array_buf: RefCell<LEDArray>,
+    info: DeviceInfo,
 }
 
 pub fn get_device_list() -> Result<DeviceList, SenselError> {
@@ -30,12 +42,6 @@ pub fn get_device_list() -> Result<DeviceList, SenselError> {
         let mut list = mem::zeroed();
         sensel_result(senselGetDeviceList(&mut list))
             .and(Ok(list.into()))
-    }
-}
-
-impl SenselSensorInfo {
-    pub fn get_num_sensors(&self) -> usize {
-        self.num_rows as usize * self.num_cols as usize
     }
 }
 
@@ -58,7 +64,13 @@ impl SenselDeviceID {
     pub fn get_com_port(&self) -> &str {
         ::std::str::from_utf8(&self.com_port).unwrap()
     }
-    pub fn open(self) -> Result<Device, SenselError> {
+    pub fn open(self) -> Result<BaseDevice, SenselError> {
+        BaseDevice::new(self)
+    }
+}
+
+impl BaseDevice {
+    fn new(id: SenselDeviceID) -> Result<Self, SenselError> {
         unsafe {
             let mut handle = mem::zeroed();
             let mut fw_info = mem::zeroed();
@@ -69,7 +81,7 @@ impl SenselDeviceID {
             let mut max_led_brightness = 0;
             let mut led_reg_size = 0;
 
-            sensel_result(senselOpenDeviceByID(&mut handle, self.idx))
+            sensel_result(senselOpenDeviceByID(&mut handle, id.idx))
                 .and_then(|_| sensel_result(senselGetFirmwareInfo(handle, &mut fw_info)))
                 .and_then(|_| sensel_result(senselGetSensorInfo(handle, &mut sensor_info)))
                 .and_then(|_| sensel_result(senselGetSupportedFrameContent(handle, &mut supported_frame_content)))
@@ -77,47 +89,116 @@ impl SenselDeviceID {
                 .and_then(|_| sensel_result(senselGetMaxLEDBrightness(handle, &mut max_led_brightness)))
                 .and_then(|_| sensel_result(senselReadReg(handle, SENSEL_REG_LED_BRIGHTNESS_SIZE as u8, SENSEL_REG_SIZE_LED_BRIGHTNESS_SIZE as u8, &mut led_reg_size)))
                 .and_then(|_| sensel_result(senselAllocateFrameData(handle, &mut frame_data)))
-                .and(Ok(Device {
+                .and(Ok(BaseDevice {
                     handle,
                     frame_data,
-                    id: self,
-                    sensor_info,
-                    fw_info,
-                    supported_frame_content: frame::Mask::from_bits_truncate(supported_frame_content),
-                    num_leds: num_leds as usize,
-                    max_led_brightness,
                     led_array_buf: RefCell::new(match led_reg_size {
                         1 => LEDArray::Char(vec![0; num_leds as usize]),
                         2 => LEDArray::Short(vec![0; num_leds as usize]),
                         _ => unimplemented!()
-                    })
+                    }),
+                    info: DeviceInfo {
+                        sensor_info,
+                        fw_info,
+                        supported_frame_content: frame::Mask::from_bits_truncate(supported_frame_content),
+                        num_leds: num_leds as usize,
+                        max_led_brightness,
+                    }
                 }))
         }
     }
-}
 
-impl Device {
+    pub fn start_scanning(self) -> Result<ScanningDevice, SenselError> {
+        unsafe {
+            sensel_result(senselStartScanning(self.handle))
+                .and(Ok(ScanningDevice(self)))
+        }
+    }
+
     pub fn soft_reset(&self) -> Result<(), SenselError> {
         unsafe {
             sensel_result(senselSoftReset(self.handle))
         }
     }
+}
 
-    pub fn get_power_button_pressed(&self) -> Result<bool, SenselError> {
+impl Drop for BaseDevice {
+    fn drop(&mut self) {
+        unsafe {
+            // always close on drop to prevent memory leaks
+            // never throws error
+            senselClose(self.handle);
+        }
+    }
+}
+
+impl Device for BaseDevice {
+    fn _get_device(&self) -> &BaseDevice {
+        self
+    }
+}
+
+impl ScanningDevice {
+    pub fn stop_scanning(self) -> Result<BaseDevice, SenselError> {
+        unsafe {
+            sensel_result(senselStopScanning(self.0.handle))
+                .and(Ok(self.0))
+        }
+    }
+
+    pub fn read_sensor(&self) -> Result<(), SenselError> {
+        unsafe {
+            sensel_result(senselReadSensor(self.0.handle))
+                .and(Ok(()))
+        }
+    }
+    pub fn get_num_available_frames(&self) -> Result<usize, SenselError> {
+        let mut num_frames = 0;
+        unsafe {
+            sensel_result(senselGetNumAvailableFrames(self.0.handle, &mut num_frames))
+                .and(Ok(num_frames as usize))
+        }
+    }
+    pub fn get_frame(&self) -> Result<frame::Frame, SenselError> {
+        unsafe {
+            sensel_result(senselGetFrame(self.0.handle, self.0.frame_data))
+                .and(Ok(frame::from_frame_data(*self.0.frame_data, self.get_info().sensor_info)))
+        }
+    }
+}
+
+impl Device for ScanningDevice {
+    fn _get_device(&self) -> &BaseDevice {
+        &self.0
+    }
+}
+
+pub trait Device where Self: ::std::marker::Sized {
+    // this should be private but not sure if possible
+    fn _get_device(&self) -> &BaseDevice;
+
+    fn get_info(&self) -> DeviceInfo {
+        self._get_device().info
+    }
+
+    fn get_power_button_pressed(&self) -> Result<bool, SenselError> {
+        let dev = self._get_device();
         let mut pressed = 0;
         unsafe {
-            sensel_result(senselGetPowerButtonPressed(self.handle, &mut pressed))
+            sensel_result(senselGetPowerButtonPressed(dev.handle, &mut pressed))
                 .and(Ok(pressed != 0))
         }
     }
 
-    pub fn set_led_array(&self, led_array: &[u16]) -> Result<(), SenselError> {
-        if led_array.len() != self.num_leds as usize {
+    fn set_led_array(&self, led_array: &[u16]) -> Result<(), SenselError> {
+        let dev = self._get_device();
+        let info = self.get_info();
+        if led_array.len() != info.num_leds {
             Err(SenselError)
-        } else if led_array.iter().any(|&brightness| brightness > self.max_led_brightness) {
+        } else if led_array.iter().any(|&brightness| brightness > info.max_led_brightness) {
             Err(SenselError)
         } else {
-            let (buf_ptr, buf_size) = match *self.led_array_buf.borrow_mut() {
+            let (buf_ptr, buf_size) = match *dev.led_array_buf.borrow_mut() {
                 LEDArray::Char(ref mut buf) => {
                     for (buf, led) in buf.iter_mut().zip(led_array) {
                         *buf = *led as u8;
@@ -128,186 +209,166 @@ impl Device {
                     buf.copy_from_slice(led_array);
                     (buf.as_mut_ptr() as *mut u8, buf.len())
                 }
-                _ => unimplemented!()
             };
             unsafe {
                 let write_size = mem::zeroed();
-                sensel_result(senselWriteRegVS(self.handle, SENSEL_REG_LED_BRIGHTNESS as u8, buf_size as u32, buf_ptr, write_size))
+                sensel_result(senselWriteRegVS(dev.handle, SENSEL_REG_LED_BRIGHTNESS as u8, buf_size as u32, buf_ptr, write_size))
                     .and(Ok(()))
             }
         }
     }
 
-    pub fn set_led_brightness(&self, led_id: u8, brightness: u16) -> Result<(), SenselError> {
+    fn set_led_brightness(&self, led_id: u8, brightness: u16) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetLEDBrightness(self.handle, led_id, brightness))
+            sensel_result(senselSetLEDBrightness(dev.handle, led_id, brightness))
         }
     }
-    pub fn get_led_brightness(&self, led_id: u8) -> Result<u16, SenselError> {
+    fn get_led_brightness(&self, led_id: u8) -> Result<u16, SenselError> {
+        let dev = self._get_device();
         let mut brightness = 0;
         unsafe {
-            sensel_result(senselGetLEDBrightness(self.handle, led_id, &mut brightness))
+            sensel_result(senselGetLEDBrightness(dev.handle, led_id, &mut brightness))
                 .and(Ok(brightness))
         }
     }
 
-    pub fn set_scan_mode(&self, mode: SenselScanMode) -> Result<(), SenselError> {
+    fn set_scan_mode(&self, mode: SenselScanMode) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetScanMode(self.handle, mode))
+            sensel_result(senselSetScanMode(dev.handle, mode))
         }
     }
-    pub fn get_scan_mode(&self) -> Result<SenselScanMode, SenselError> {
+    fn get_scan_mode(&self) -> Result<SenselScanMode, SenselError> {
+        let dev = self._get_device();
         let mut mode = SenselScanMode::SCAN_MODE_DISABLE;
         unsafe {
-            sensel_result(senselGetScanMode(self.handle, &mut mode))
+            sensel_result(senselGetScanMode(dev.handle, &mut mode))
                 .and(Ok(mode))
         }
     }
 
-    pub fn set_scan_detail(&self, detail: SenselScanDetail) -> Result<(), SenselError> {
+    fn set_scan_detail(&self, detail: SenselScanDetail) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetScanDetail(self.handle, detail))
+            sensel_result(senselSetScanDetail(dev.handle, detail))
         }
     }
-    pub fn get_scan_detail(&self) -> Result<SenselScanDetail, SenselError> {
+    fn get_scan_detail(&self) -> Result<SenselScanDetail, SenselError> {
+        let dev = self._get_device();
         let mut detail = SenselScanDetail::SCAN_DETAIL_UNKNOWN;
         unsafe {
-            sensel_result(senselGetScanDetail(self.handle, &mut detail))
+            sensel_result(senselGetScanDetail(dev.handle, &mut detail))
                 .and(Ok(detail))
         }
     }
 
-    pub fn set_buffer_control(&self, num: u8) -> Result<(), SenselError> {
+    fn set_buffer_control(&self, num: u8) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetBufferControl(self.handle, num))
+            sensel_result(senselSetBufferControl(dev.handle, num))
         }
     }
-    pub fn get_buffer_control(&self) -> Result<u8, SenselError> {
+    fn get_buffer_control(&self) -> Result<u8, SenselError> {
+        let dev = self._get_device();
         let mut num = 0;
         unsafe {
-            sensel_result(senselGetBufferControl(self.handle, &mut num))
+            sensel_result(senselGetBufferControl(dev.handle, &mut num))
                 .and(Ok(num))
         }
     }
 
-    pub fn set_max_frame_rate(&self, val: u16) -> Result<(), SenselError> {
+    fn set_max_frame_rate(&self, val: u16) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetMaxFrameRate(self.handle, val))
+            sensel_result(senselSetMaxFrameRate(dev.handle, val))
         }
     }
-    pub fn get_max_frame_rate(&self) -> Result<u16, SenselError> {
+    fn get_max_frame_rate(&self) -> Result<u16, SenselError> {
+        let dev = self._get_device();
         let mut val = 0;
         unsafe {
-            sensel_result(senselGetMaxFrameRate(self.handle, &mut val))
+            sensel_result(senselGetMaxFrameRate(dev.handle, &mut val))
                 .and(Ok(val))
         }
     }
 
-    pub fn set_frame_content(&self, mask: frame::Mask) -> Result<(), SenselError> {
+    fn set_frame_content(&self, mask: frame::Mask) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetFrameContent(self.handle, mask.bits()))
+            sensel_result(senselSetFrameContent(dev.handle, mask.bits()))
         }
     }
-    pub fn get_frame_content(&self) -> Result<frame::Mask, SenselError> {
+    fn get_frame_content(&self) -> Result<frame::Mask, SenselError> {
+        let dev = self._get_device();
         let mut mask = 0;
         unsafe {
-            sensel_result(senselGetFrameContent(self.handle, &mut mask))
+            sensel_result(senselGetFrameContent(dev.handle, &mut mask))
                 .and(Ok(frame::Mask::from_bits_truncate(mask)))
         }
     }
 
-    pub fn set_contacts_mask(&self, mask: contact::Mask) -> Result<(), SenselError> {
+    fn set_contacts_mask(&self, mask: contact::Mask) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetContactsMask(self.handle, mask.bits()))
+            sensel_result(senselSetContactsMask(dev.handle, mask.bits()))
         }
     }
-    pub fn get_contacts_mask(&self) -> Result<contact::Mask, SenselError> {
+    fn get_contacts_mask(&self) -> Result<contact::Mask, SenselError> {
+        let dev = self._get_device();
         let mut mask = 0;
         unsafe {
-            sensel_result(senselGetContactsMask(self.handle, &mut mask))
+            sensel_result(senselGetContactsMask(dev.handle, &mut mask))
                 .and(Ok(contact::Mask::from_bits_truncate(mask)))
         }
     }
 
-    pub fn set_contacts_min_force(&self, val: u16) -> Result<(), SenselError> {
+    fn set_contacts_min_force(&self, val: u16) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetContactsMinForce(self.handle, val))
+            sensel_result(senselSetContactsMinForce(dev.handle, val))
         }
     }
-    pub fn get_contacts_min_force(&self) -> Result<u16, SenselError> {
+    fn get_contacts_min_force(&self) -> Result<u16, SenselError> {
+        let dev = self._get_device();
         let mut val = 0;
         unsafe {
-            sensel_result(senselGetContactsMinForce(self.handle, &mut val))
+            sensel_result(senselGetContactsMinForce(dev.handle, &mut val))
                 .and(Ok(val))
         }
     }
 
-    pub fn set_contacts_enable_blob_merge(&self, val: bool) -> Result<(), SenselError> {
+    fn set_contacts_enable_blob_merge(&self, val: bool) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetContactsEnableBlobMerge(self.handle, val as u8))
+            sensel_result(senselSetContactsEnableBlobMerge(dev.handle, val as u8))
         }
     }
-    pub fn get_contacts_enable_blob_merge(&self) -> Result<bool, SenselError> {
+    fn get_contacts_enable_blob_merge(&self) -> Result<bool, SenselError> {
+        let dev = self._get_device();
         let mut val = 0;
         unsafe {
-            sensel_result(senselGetContactsEnableBlobMerge(self.handle, &mut val))
+            sensel_result(senselGetContactsEnableBlobMerge(dev.handle, &mut val))
                 .and(Ok(val != 0))
         }
     }
 
-    pub fn set_dynamic_baseline_enabled(&self, val: bool) -> Result<(), SenselError> {
+    fn set_dynamic_baseline_enabled(&self, val: bool) -> Result<(), SenselError> {
+        let dev = self._get_device();
         unsafe {
-            sensel_result(senselSetDynamicBaselineEnabled(self.handle, val as u8))
+            sensel_result(senselSetDynamicBaselineEnabled(dev.handle, val as u8))
         }
     }
-    pub fn get_dynamic_baseline_enabled(&self) -> Result<bool, SenselError> {
+    fn get_dynamic_baseline_enabled(&self) -> Result<bool, SenselError> {
+        let dev = self._get_device();
         let mut val = 0;
         unsafe {
-            sensel_result(senselGetDynamicBaselineEnabled(self.handle, &mut val))
+            sensel_result(senselGetDynamicBaselineEnabled(dev.handle, &mut val))
                 .and(Ok(val != 0))
         }
     }
 
-    pub fn start_scanning(&self) -> Result<(), SenselError> {
-        unsafe {
-            sensel_result(senselStartScanning(self.handle))
-        }
-    }
-    pub fn stop_scanning(&self) -> Result<(), SenselError> {
-        unsafe {
-            sensel_result(senselStopScanning(self.handle))
-        }
-    }
-
-    pub fn read_sensor(&self) -> Result<(), SenselError> {
-        unsafe {
-            sensel_result(senselReadSensor(self.handle))
-        }
-    }
-    pub fn get_num_available_frames(&self) -> Result<usize, SenselError> {
-        let mut num_frames = 0;
-        unsafe {
-            sensel_result(senselGetNumAvailableFrames(self.handle, &mut num_frames))
-                .and(Ok(num_frames as usize))
-        }
-    }
-    pub fn get_frame(&self) -> Result<frame::Frame, SenselError> {
-        unsafe {
-            sensel_result(senselGetFrame(self.handle, self.frame_data))
-                .and(Ok(frame::from_frame_data(*self.frame_data, self.sensor_info)))
-        }
-    }
-    pub fn close(self) -> () {
+    fn close(self) -> () {
         // move and drop the device
-    }
-}
-
-impl Drop for Device {
-    fn drop(&mut self) {
-        unsafe {
-            // always close on drop to prevent memory leaks
-            // never throws error
-            senselClose(self.handle);
-        }
     }
 }
